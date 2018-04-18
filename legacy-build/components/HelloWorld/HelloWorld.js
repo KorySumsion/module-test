32d5768df4561c5ef8365779e608de504ece385b
korysumsion <korysumsion@gmail.com>
2018-04-18 11:46:17 -0600

    Publish v0.1.4 -- with build and legacy-build

---
+++ legacy-build/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,28 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var _jsxFileName = 'src/components/HelloWorld/HelloWorld.jsx';
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var HelloWorld = function HelloWorld() {
+  return _react2.default.createElement(
+    'div',
+    {
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 5
+      },
+      __self: undefined
+    },
+    'Hello World \uD83E\uDD84 Brian guy'
+  );
+};
+
+exports.default = HelloWorld;
