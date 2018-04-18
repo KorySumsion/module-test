5386749040aa3a5a5e7c9304bab3004420e1f899
korysumsion <korysumsion@gmail.com>
2018-04-18 10:27:30 -0600

    Publish v0.1.3 -- with build and legacy-build

---
+++ legacy-build/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _HelloWorld = require('./components/HelloWorld/HelloWorld');
+
+var _HelloWorld2 = _interopRequireDefault(_HelloWorld);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _HelloWorld2.default;
