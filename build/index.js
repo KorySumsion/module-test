9af77cb2ee585e41bc2a499780dec26705e30a09
korysumsion <korysumsion@gmail.com>
2018-04-18 10:13:26 -0600

    Publish v0.1.1 -- with build and legacy-build

---
+++ build/index.js
@@ -0,0 +1,3 @@
+import HelloWorld from './components/HellowWorld/HellowWorld';
+
+export default HelloWorld;
---
+++ legacy-build/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _HellowWorld = require('./components/HellowWorld/HellowWorld');
+
+var _HellowWorld2 = _interopRequireDefault(_HellowWorld);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _HellowWorld2.default;
