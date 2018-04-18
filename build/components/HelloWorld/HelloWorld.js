e7bb1f17f26c034c812f1ec47199f0b9b90e04e1
korysumsion <korysumsion@gmail.com>
2018-04-18 10:21:52 -0600

    Publish v0.1.2 -- with build and legacy-build

---
+++ build/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,20 @@
+var _jsxFileName = 'src/components/HelloWorld/HelloWorld.jsx',
+    _this = this;
+
+import React from 'react';
+
+var HelloWorld = function HelloWorld() {
+  return React.createElement(
+    'div',
+    {
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 5
+      },
+      __self: _this
+    },
+    'Hello World'
+  );
+};
+
+export default HellowWorld;
